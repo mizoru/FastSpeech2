ffc78071c104158b4e06e2f820fd9480110a82a5
mizoru <svmaxim2001@gmail.com>
2022-11-27 04:33:01 +0300

    ^^

--- src/model/FastSpeech2.py
+++ src/model/FastSpeech2.py
@@ -236,3 +236,194 @@ class LengthRegulator(nn.Module):
             output = self.LR(x, duration_predictor_output, mel_max_length)
             mel_pos = torch.stack([torch.tensor([i+1 for i in range(output.size(1))], device=train_config.device, dtype=torch.int64)])
             return output, mel_pos
+        
+
+class VarianceAdaptor(nn.Module):
+    """ Variance Adaptor """
+
+    def __init__(self, model_config, stats):
+        super(VarianceAdaptor, self).__init__()
+        self.pitch_predictor = VariancePredictor(model_config)
+        self.energy_predictor = VariancePredictor(model_config)
+        self.register_buffer("pitch_bins", torch.exp(
+            torch.linspace(torch.log(stats["f0"]["min"]), torch.log(stats["f0"]["max"]), model_config.quantization_bins-1)))
+        self.register_buffer("energy_bins", torch.exp(
+            torch.linspace(torch.log(stats["energy"]["min"]), torch.log(stats["energy"]["max"]), model_config.quantization_bins-1)))
+        self.pitch_embed = nn.Embedding(model_config.quantization_bins, model_config.encoder_dim)
+        self.energy_embed = nn.Embedding(model_config.quantization_bins, model_config.encoder_dim)
+
+    def forward(self, x, beta=1.0, gamma=1.0, pitch=None, energy=None):
+        pitch_pred = self.pitch_predictor(x)
+        energy_pred = self.pitch_predictor(x)
+        if pitch is not None and energy is not None:
+            pitch = torch.bucketize(pitch, self.pitch_bins)
+            energy = torch.bucketize(pitch, self.energy_bins)
+            x = x + self.pitch_embed(pitch)
+            x = x + self.pitch_embed(energy)
+            return x, pitch_pred, energy_pred
+        elif pitch is None and energy is None:
+            pitch = torch.bucketize(pitch_pred*beta, self.pitch_bins)
+            energy = torch.bucketize(energy_pred*gamma, self.energy_bins)
+            x = x + self.pitch_embed(pitch)
+            x = x + self.pitch_embed(energy)
+            return x
+        else:
+            raise Exception("Either both or none of energy, pitch")
+        
+def get_non_pad_mask(seq):
+    assert seq.dim() == 2
+    return seq.ne(model_config.PAD).type(torch.float).unsqueeze(-1)
+
+def get_attn_key_pad_mask(seq_k, seq_q):
+    ''' For masking out the padding part of key sequence. '''
+    # Expand to fit the shape of key query attention matrix.
+    len_q = seq_q.size(1)
+    padding_mask = seq_k.eq(model_config.PAD)
+    padding_mask = padding_mask.unsqueeze(
+        1).expand(-1, len_q, -1).unsqueeze(1)  # b x 1 x lq x lk
+
+    return padding_mask
+
+class Encoder(nn.Module):
+    def __init__(self, model_config):
+        super(Encoder, self).__init__()
+        
+        len_max_seq=model_config.max_seq_len
+        n_position = len_max_seq + 1
+        n_layers = model_config.encoder_n_layer
+
+        self.src_word_emb = nn.Embedding(
+            model_config.vocab_size,
+            model_config.encoder_dim,
+            padding_idx=model_config.PAD
+        )
+
+        self.position_enc = nn.Embedding(
+            n_position,
+            model_config.encoder_dim,
+            padding_idx=model_config.PAD
+        )
+
+        self.layer_stack = nn.ModuleList([FFTBlock(
+            model_config.encoder_dim,
+            model_config.encoder_conv1d_filter_size,
+            model_config.encoder_head,
+            dropout=model_config.dropout
+        ) for _ in range(n_layers)])
+
+    def forward(self, src_seq, src_pos, return_attns=False):
+
+        enc_slf_attn_list = []
+
+        # -- Prepare masks
+        slf_attn_mask = get_attn_key_pad_mask(seq_k=src_seq, seq_q=src_seq)
+        non_pad_mask = get_non_pad_mask(src_seq)
+        
+        # -- Forward
+        enc_output = self.src_word_emb(src_seq) + self.position_enc(src_pos)
+
+        for enc_layer in self.layer_stack:
+            enc_output, enc_slf_attn = enc_layer(
+                enc_output,
+                non_pad_mask=non_pad_mask,
+                slf_attn_mask=slf_attn_mask)
+            if return_attns:
+                enc_slf_attn_list += [enc_slf_attn]
+        
+
+        return enc_output, non_pad_mask
+    
+    
+class Decoder(nn.Module):
+    """ Decoder """
+
+    def __init__(self, model_config):
+
+        super(Decoder, self).__init__()
+
+        len_max_seq=model_config.max_seq_len
+        n_position = len_max_seq + 1
+        n_layers = model_config.decoder_n_layer
+
+        self.position_enc = nn.Embedding(
+            n_position,
+            model_config.encoder_dim,
+            padding_idx=model_config.PAD,
+        )
+
+        self.layer_stack = nn.ModuleList([FFTBlock(
+            model_config.encoder_dim,
+            model_config.encoder_conv1d_filter_size,
+            model_config.encoder_head,
+            dropout=model_config.dropout
+        ) for _ in range(n_layers)])
+
+    def forward(self, enc_seq, enc_pos, return_attns=False):
+
+        dec_slf_attn_list = []
+
+        # -- Prepare masks
+        slf_attn_mask = get_attn_key_pad_mask(seq_k=enc_pos, seq_q=enc_pos)
+        non_pad_mask = get_non_pad_mask(enc_pos)
+
+        # -- Forward
+        dec_output = enc_seq + self.position_enc(enc_pos)
+
+        for dec_layer in self.layer_stack:
+            dec_output, dec_slf_attn = dec_layer(
+                dec_output,
+                non_pad_mask=non_pad_mask,
+                slf_attn_mask=slf_attn_mask)
+            if return_attns:
+                dec_slf_attn_list += [dec_slf_attn]
+
+        return dec_output
+
+
+def get_mask_from_lengths(lengths, max_len=None):
+    if max_len == None:
+        max_len = torch.max(lengths).item()
+
+    ids = torch.arange(0, max_len, 1, device=lengths.device)
+    mask = (ids < lengths.unsqueeze(1)).bool()
+
+    return mask
+
+class FastSpeech(nn.Module):
+    """ FastSpeech """
+
+    def __init__(self, model_config, stats):
+        super(FastSpeech, self).__init__()
+
+        self.encoder = Encoder(model_config)
+        self.length_regulator = LengthRegulator(model_config)
+        self.variance_adaptor = VarianceAdaptor(model_config, stats)
+        self.decoder = Decoder(model_config)
+
+        self.mel_linear = nn.Linear(model_config.decoder_dim, mel_config.num_mels)
+
+    def mask_tensor(self, mel_output, position, mel_max_length):
+        lengths = torch.max(position, -1)[0]
+        mask = ~get_mask_from_lengths(lengths, max_len=mel_max_length)
+        mask = mask.unsqueeze(-1).expand(-1, -1, mel_output.size(-1))
+        return mel_output.masked_fill(mask, 0.)
+
+    def forward(self, src_seq, src_pos, mel_pos=None, mel_max_length=None, length_target=None,
+                pitch_target=None, energy_target=None, alpha=1.0, beta=1.0, gamma=1.0):
+        x, non_pad_mask = self.encoder(src_seq, src_pos)
+
+        if self.training:
+            output, duration_predictor_output = self.length_regulator(x, alpha, length_target, mel_max_length)
+            output, pitch_prediction, energy_prediction = self.variance_adaptor(output, beta=beta, gamma=gamma, pitch=pitch_target, energy=energy_target)
+            output = self.decoder(output, mel_pos)
+
+            output = self.mask_tensor(output, mel_pos, mel_max_length)
+            
+            output = self.mel_linear(output)
+            return output, duration_predictor_output, pitch_prediction, energy_prediction
+        else:
+            output, mel_pos = self.length_regulator(x, alpha)
+            output = self.variance_adaptor(self, output, beta=beta, gamma=gamma)
+            output = self.decoder(output, mel_pos)
+            output = self.mel_linear(output)
+            return output
